237dfcae2f7abcccaea7935efc80e0207791a51e
Paulo Santos <psantos@blackducksoftware.com>
2016-07-25 12:08:52 -0400

    Add the ability to generate a properties file or read existing one.

--- src/main/java/com/blackducksoftware/integration/email/Application.java
+++ src/main/java/com/blackducksoftware/integration/email/Application.java
@@ -1,19 +1,45 @@
 package com.blackducksoftware.integration.email;
 
+import java.io.IOException;
 import java.util.Properties;
 
+import org.apache.commons.cli.CommandLine;
+import org.apache.commons.cli.CommandLineParser;
+import org.apache.commons.cli.DefaultParser;
+import org.apache.commons.cli.Option;
+import org.apache.commons.cli.Options;
+import org.apache.commons.cli.ParseException;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
 import com.blackducksoftware.integration.email.service.ConfigurationResponseParser;
+import com.blackducksoftware.integration.email.service.properties.ServicePropertiesBuilder;
 import com.google.gson.Gson;
 
 @SpringBootApplication
 public class Application {
+
+	public final static String ARG_PROP_FILE = "propertyFile";
+
 	public static void main(final String[] args) {
-		SpringApplication.run(Application.class, args);
+		try {
+			final CommandLine cmd = parseCommandLine(args);
+			final ServicePropertiesBuilder propertyBuilder = new ServicePropertiesBuilder();
+
+			if (cmd.hasOption(ARG_PROP_FILE)) {
+				final String path = cmd.getOptionValue(ARG_PROP_FILE);
+				propertyBuilder.setFilePath(path);
+			}
+
+			final Properties props = propertyBuilder.build();
+			System.out.println("Properties: " + props);
+
+			SpringApplication.run(Application.class, args);
+		} catch (final ParseException | IOException e) {
+			e.printStackTrace();
+		}
 	}
 
 	@Bean
@@ -31,13 +57,19 @@ public class Application {
 		return new ConfigurationResponseParser();
 	}
 
-	private boolean doesPropertiesExist(final String path) {
-		return true;
+	private static Options createCommandLineOptions() {
+		final Options options = new Options();
+		final Option option = new Option(ARG_PROP_FILE, true,
+				"The path to the file containing the properties for the service.");
+		option.setArgName("file");
+
+		options.addOption(option);
+		return options;
 	}
 
-	private void generatePropertiesFile() {
-		final Properties properties = new Properties();
-		properties.setProperty("", "");
-
+	private static CommandLine parseCommandLine(final String[] args) throws ParseException {
+		final Options options = createCommandLineOptions();
+		final CommandLineParser parser = new DefaultParser();
+		return parser.parse(options, args);
 	}
 }
