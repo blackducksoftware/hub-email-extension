acb677650291b4cd9a3cbf9ece848b8979408750
Rob Pacheco <rpacheco@blackducksoftware.com>
2017-11-15 08:59:51 -0500

    Updating dockerfile and entry point to automatically import certificates from hub and custom certificates

--- Dockerfile
+++ Dockerfile
@@ -10,7 +10,7 @@ ENV PATH $EMAIL_EXT_HOME/bin:$PATH
 
 RUN set -e \
     # The old version of the Gradle Application plugin generates Bash scripts
-    && apk add --no-cache --virtual .hub-ext-rundeps bash \
+    && apk add --no-cache --virtual .hub-ext-rundeps bash curl jq openssl \
     && addgroup -S hubext \
     && adduser -h "$EMAIL_EXT_HOME" -g hubext -s /sbin/nologin -G hubext -S -D -H hubext
 
--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -18,30 +18,61 @@ verifyEnvironment() {
   fi
 }
 
-importCertificate(){
-echo "Attempting to import Hub Certificate"
-echo $PUBLIC_HUB_WEBSERVER_HOST
-echo $PUBLIC_HUB_WEBSERVER_PORT
+importCertificate() {
+  echo "Attempting to import Hub Root Certificate"
 
-# In case of email-extension container restart
-if keytool -list -keystore "$JAVA_HOME/lib/security/cacerts" -storepass changeit -alias publichubwebserver >/dev/null
-then
-    keytool -delete -alias publichubwebserver -keystore "$JAVA_HOME/lib/security/cacerts" -storepass changeit
-   	echo "Removing the existing certificate after container restart"
-fi
+  # In case of email-extension container restart
+  # Remove hub root
+  if keytool -list -keystore "$JAVA_HOME/lib/security/cacerts" -storepass changeit -alias hubrootcertificate >/dev/null
+  then
+    keytool -delete -alias hubrootcertificate -keystore "$JAVA_HOME/lib/security/cacerts" -storepass changeit
+   	echo "Removing the hub web root existing certificate after container restart"
+  fi
 
-#Workaround as the hub refresh token is not usable
-if [ -f $EMAIL_EXT_HOME/config/oauth.properties ] 
-then
-	rm $EMAIL_EXT_HOME/config/oauth.properties
-fi
+  # Remove custom cert
+  if keytool -list -keystore "$JAVA_HOME/lib/security/cacerts" -storepass changeit -alias customhubservercertificate >/dev/null
+  then
+    keytool -delete -alias customhubservercertificate -keystore "$JAVA_HOME/lib/security/cacerts" -storepass changeit
+   	echo "Removing the existing custom hub web server certificate after container restart"
+  fi
 
-if keytool -printcert -rfc -sslserver "$PUBLIC_HUB_WEBSERVER_HOST:$PUBLIC_HUB_WEBSERVER_PORT" -v | keytool -importcert -keystore "$JAVA_HOME/lib/security/cacerts" -storepass changeit -alias publichubwebserver -noprompt > /dev/null 
-then
-	echo "Completed importing Hub Certificate"
-else
-	echo "Unable to add the certificate. Please try to import the certificate manually."
-fi
+  targetCAHost="${HUB_CFSSL_HOST:-cfssl}"
+  targetCAPort="${HUB_CFSSL_PORT:-8888}"
+  url="http://$targetCAHost:$targetCAPort/api/v1/cfssl/info"
+  export body="{ \"label\": \"root\", \"profile\": \"peer\" }"
+
+  echo "Attempting to get root certificate."
+  echo "URL: $url"
+  echo "Body: $body"
+
+  curl -X POST -H "Content-Type: application/json" -d "$body" -o ./root.json --connect-timeout 15 --max-time 30 --retry 20 --retry-delay 10 --retry-max-time 180 --retry-connrefused $url
+
+  cat root.json |   jq '.result.certificate' | tr -d '"' | sed 's/\\n/\
+/g' > root.crt
+
+  if cat root.crt  | keytool -importcert -keystore "$JAVA_HOME/lib/security/jssecacerts" -storepass changeit -alias hubrootcertificate -noprompt > /dev/null
+  then
+	echo "Completed importing Hub Root Certificate"
+  else
+	echo "Unable to add the hub root certificate. Please try to import the certificate manually."
+  fi
+
+  if [ -f /run/secrets/WEBSERVER_CUSTOM_CERT_FILE ]; then
+    if cat /run/secrets/WEBSERVER_CUSTOM_CERT_FILE  | keytool -importcert -keystore "$JAVA_HOME/lib/security/jssecacerts" -storepass changeit -alias hcustomhubservercertificate -noprompt > /dev/null
+    then
+	  echo "Completed importing Custom Hub Web Server Certificate"
+    else
+	  echo "Unable to add the custom hub web server certificate. Please try to import the certificate manually."
+    fi
+  else
+    echo "Custom Hub Web Server Certificate not specified, skipping."
+  fi
+
+  #Workaround as the hub refresh token is not usable
+  if [ -f $EMAIL_EXT_HOME/config/oauth.properties ]
+  then
+  	rm $EMAIL_EXT_HOME/config/oauth.properties
+  fi
 }
 
 
